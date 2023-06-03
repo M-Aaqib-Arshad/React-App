1e8e0a59ea9373cf9963f437065337efc98f7315
Muhammad Aaqib Arshad <muhammad.aaqib.arshad@gmail.com>
2023-06-03 20:29:57 +0500

    Project working well till end header.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.scss';
+import "..//node_modules/bootstrap/dist/js/bootstrap.bundle";
+// Components
+import Header from'./Components/Header';
+import Footer from './Components/Footer';
+
+//Pages
+import Home from './Pages/Home'
+
+function App() {
+  return (
+    <>
+     <header>
+     <Header />
+     </header>
+     <main>
+     
+     </main>
+     <Footer />
+    </>
+  );
+}
+
+export default App;
