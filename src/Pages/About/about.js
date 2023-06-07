ce0982487a58227119ec1a3ec0095d2a03b9c43d
Muhammad Aaqib Arshad <muhammad.aaqib.arshad@gmail.com>
2023-06-08 00:12:59 +0500

    Login also created

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -4,7 +4,8 @@ export default function () {
   return (
     <>
        <h1 className='text-center' style={{font:100}}>About</h1>
-    <h1 className='text-center'>No date found</h1>
+    <h1 className='text-center'>Our Team found many Project</h1>
+    
     </>
   )
 }
